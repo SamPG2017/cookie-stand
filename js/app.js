7cf5a616c779b70153bcd46337c694049236f31c
Sam Porter Giffords <travelinsamp@gmail.com>
2018-03-12 16:03:27 -0700

    Got the function attached to the object and function works

--- js/app.js
+++ js/app.js
@@ -1,70 +1,87 @@
 "use strict"
 
+
 // Create all object literals for five stores provided, using information provided along with names
 var storeDetailsPike = {
   storeNumber: "1",
   storeLocation: '1st and Pike',
   minCustPerHourPike: 23,
   maxCustPerHourPike: 65,
-  avgCookiePerCustPike: 6.3
+  avgCookiePerCustPike: 6.3,
+  
+  getRandomIntInclusive: function () { 
+    return Math.floor(Math.random() * (this.maxCustPerHourPike - this.minCustPerHourPike + 1)) + this.minCustPerHourPike; //The maximum is inclusive and the minimum is inclusive 
+  }
 }
-console.log('Store Details Pike: ', storeDetailsPike);
+//console.log('Store Details Pike: ', storeDetailsPike);
+console.log('RandomPike: ', storeDetailsPike.getRandomIntInclusive());
+
 
 var storeDetailsSeaTac = {
   storeNumber: "2",
   storeLocation: 'SeaTac Airport',
   minCustPerHourSeaTac: 3,
   maxCustPerHourSeaTac: 24,
-  avgCookiePerCustSeaTac: 1.2
-}
+  avgCookiePerCustSeaTac: 1.2,
 
-console.log('Store Details SeaTac: ', storeDetailsSeaTac);
+  getRandomIntInclusive: function () { 
+    return Math.floor(Math.random() * (this.maxCustPerHourSeaTac - this.minCustPerHourSeaTac + 1)) + this.minCustPerHourSeaTac; //The maximum is inclusive and the minimum is inclusive 
+  }
+}
+//console.log('Store Details SeaTac: ', storeDetailsSeaTac);
+console.log('RandomSeaTac: ', storeDetailsSeaTac.getRandomIntInclusive());
 
 var storeDetailsSeaCtr = {
   storeNumber: '3',
   storeLocation: 'Seattle Center',
   minCustPerHourSeaCtr: 11,
   maxCustPerHourSeaCtr: 38,
-  avgCookiePerCustSeaCtr: 3.7
+  avgCookiePerCustSeaCtr: 3.7, 
+  
+   getRandomIntInclusive: function () { 
+    return Math.floor(Math.random() * (this.maxCustPerHourSeaCtr - this.minCustPerHourSeaCtr + 1)) + this.minCustPerHourSeaCtr; //The maximum is inclusive and the minimum is inclusive 
+  }
 }
-
-console.log('Store Details SeaCtr: ', storeDetailsSeaCtr);
+//console.log('Store Details SeaCtr: ', storeDetailsSeaCtr);
+console.log('RandomSeaCtr: ', storeDetailsSeaTac.getRandomIntInclusive());
 
 var storeDetailsCapHill = {
   storeNumber: '4',
   storeLocation: 'Capitol Hill',
   minCustPerHourCapHill: 20,
   maxCustPerHourCapHill: 38,
-  avgCookiePerCustCapHill: 2.3
+  avgCookiePerCustCapHill: 2.3,
+  
+  getRandomIntInclusive: function () { 
+    return Math.floor(Math.random() * (this.maxCustPerHourSeaCtr - this.minCustPerHourSeaCtr + 1)) + this.minCustPerHourSeaCtr; //The maximum is inclusive and the minimum is inclusive 
+  }
 }
+//console.log('Store Details CapHill: ', storeDetailsCapHill);
+console.log('RandomCapHill: ', storeDetailsSeaTac.getRandomIntInclusive());
 
-console.log('Store Details CapHill: ', storeDetailsCapHill);
 
 var storeDetailsAlki = {
   storeNumber: '5',
   storeLocation: 'Alki',
   minCustPerHour: 2,
   maxCustPerHour: 16,
-  avgCookiePerCust: 4.6
+  avgCookiePerCust: 4.6,
+  
+  getRandomIntInclusive: function () { 
+    return Math.floor(Math.random() * (this.maxCustPerHourSeaCtr - this.minCustPerHourSeaCtr + 1)) + this.minCustPerHourSeaCtr; //The maximum is inclusive and the minimum is inclusive 
+  }
 }
+// console.log('Store Details Alki: ', storeDetailsAlki);
+console.log('RandomAlki: ', storeDetailsSeaTac.getRandomIntInclusive());
 
- console.log('Store Details Alki: ', storeDetailsAlki);
- /*
-var minCustPerHour = 2;
-var maxCustPerHour = 16;
 
-function getRandomIntInclusive (storeDetailsAlki.minCustPerHour, maxCustPerHour) {
-  min = Math.ceil(minCustPerHour);
-  max = Math.floor(maxCustPerHour);
-  console.log('Min is equal to: ', minCustPerHour, ' and max is equal to: ', maxCustPerHour)
-  return Math.floor(Math.random() * (maxCustPerHour - minCustPerHour + 1)) + minCustPerHour;
-}
-getRandomIntInclusive(minCustPerHour,maxCustPerHour);
+ // calculateRandom: function () {
+//    return this.Math.floor(Math.random() * (storeDetailsAlki.maxCustPerHour //- storeDetailAlki.minCustPerHour + 1)) + storeDetailAlki.minCustPerHour;
+//}
 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 }
-*/
-
+getRandomIntInclusive(2,16);
